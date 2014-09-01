99596394617ecc69ba95eadaec9ee4e588dba0af
Andy Barry <abarry@gmail.com>
2014-09-01 18:20:13 -0400

    First working (as in it runs and doesnt crash) position controlled, massless control surfaces.

--- runModel.m
+++ runModel.m
@@ -3,7 +3,15 @@ options.floating = true;
 r = RigidBodyManipulator('urdf/robots/TBSC_assembly_urdf2.URDF', options);
 v = r.constructVisualizer;
 
-x0 = [0; 0; 0; 0; 0; 0; 0; 0; 0; 10; 0; 0; 0; 0; 0; 0; 0; 0];
-[ytraj, xtraj] = r.simulate([0 1], x0);
+%%
+
+x0 = [0; 0; 0; 0; 0; 0; 10; 0; 0; 0; 0; 0];
+
+constant_traj = ConstantTrajectory([-pi/2 -pi/2 0]);
+constant_traj = constant_traj.setOutputFrame(r.getInputFrame());
+
+feedback_system = cascade(constant_traj, r);
+
+[ytraj, xtraj] = feedback_system.simulate([0 1], x0);
 
 v.playback(xtraj)
