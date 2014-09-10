9273bf63aa14ca4fffd2a52ce70fac9c8e155be4
Andy Barry <abarry@gmail.com>
2014-09-10 15:34:33 -0400

    Updates URDF for correct parameters from solidworks.

--- runModel.m
+++ runModel.m
@@ -10,15 +10,14 @@ disp('Simulating...');
 
 x0 = [0; 0; 0; 0; 0; 0; 10; 0; 0; 0; 0; 0];
 
-constant_traj = ConstantTrajectory([.855 .855 100]);
+constant_traj = ConstantTrajectory([0.1 0.1 100]);
 constant_traj = constant_traj.setOutputFrame(r.getInputFrame());
 
 feedback_system = cascade(constant_traj, r);
 
-feedback_visualize_system = cascade(feedback_system, v);
+%feedback_visualize_system = cascade(feedback_system, v);
+%feedback_visualize_system.simulate([0 1], x0);
 
+[ytraj, xtraj] = feedback_system.simulate([0 1], x0);
 
-
-feedback_visualize_system.simulate([0 1], x0);
-
-%v.playback(xtraj)
+v.playback(xtraj)
