c96f6cc6ab776336645956cefe6427412a7607f1
Andy Barry <abarry@gmail.com>
2014-10-28 17:27:22 -0400

    Model stuff, keeping up with drake branch changes.

--- runModel.m
+++ runModel.m
@@ -3,7 +3,8 @@ disp('Constructing from URDF...');
 options.floating = true;
 r = RigidBodyManipulator('urdf/robots/TBSC_assembly_urdf2_params.URDF', options);
 %r = RigidBodyManipulator('/home/abarry/drake/drake/examples/Wingeron/Plane.URDF', options);
-v = r.constructVisualizer();
+
+
 
 %r = r.setParamFrame(CoordinateFrame('TbscParams',2,'p',...
 %        { 'drag_fuselage_xy','drag_fuselage_z' }));
@@ -11,34 +12,33 @@ v = r.constructVisualizer();
 params = r.getParams();
 
 params.drag_fuselage_xy = 1;
+params.drag_fuselage_z = 100000;
 
 r = r.setParams(params);
 
-q = zeros(r.getNumContStates(),1);
-qd = zeros(r.getNumContStates(),1);
 
-clf
-for i = 1 : length(r.force)
-  plot3(0, 0, 0, '*');
-  if isa(r.force{i}, 'RigidBodyWing')
-    r.force{i}.drawWing(r, q, qd, [rand() rand() rand()]);
-  end
-end
+v = r.constructVisualizer();
+
+v.inspector(zeros(12,1), 1:12)
 
 %%
 
-disp('Simulating...');
+%    [x; y; z; roll; pitch; yaw; xdot; ydot; zdot; rolldot; pitchdot; yawdot]
+x0 = [0; 0; 0; 0; 0; 0; .1; 0; 0; 0; 0; 0];
 
-x0 = [0; 0; 0; 0; 0; 0; 15; 0; 0; 0; 0; 0];
-
-constant_traj = ConstantTrajectory([-.8 .8 100]);
+constant_traj = ConstantTrajectory([0.8 0.8 100]);
 constant_traj = constant_traj.setOutputFrame(r.getInputFrame());
 
 feedback_system = cascade(constant_traj, r);
+
+
+disp('Simulating...');
+
 
 %feedback_visualize_system = cascade(feedback_system, v);
 %feedback_visualize_system.simulate([0 1], x0);
 
 [ytraj, xtraj] = feedback_system.simulate([0 1], x0);
 
-v.playback(xtraj)
+options_v.slider = true;
+v.playback(xtraj, options_v)
