586d893e950ffd74b134b109c1ce3b9066161d78
Andy Barry <abarry@gmail.com>
2015-03-03 19:43:45 -0500

    Adds body drag x to sysid.

--- tbsc_model.m
+++ tbsc_model.m
@@ -1,4 +1,4 @@
-function [xdot, y] = tbsc_model(t,x,u, Jx_fac, Jy_fac, Jz_fac, elev_lift_fac, elev_drag_fac, varargin) % Jx_fac,Jy_fac,Jz_fac,elev_lift_fac,F_Q_fac_x,F_Q_fac_z,thr_to_sp_ail,thr_vel_fac_ail,thr_to_sp_elev,thr_vel_fac_elev,varargin) % M_P_fac,M_Q_fac,M_R_fac,varargin) 
+function [xdot, y] = tbsc_model(t,x,u, Jx_fac, Jy_fac, Jz_fac, elev_lift_fac, elev_drag_fac, body_x_drag_fac, varargin)
 % Model derived from Ani's SBach model
 
 % Set output (first six states)
--- tbsc_model_pem_wrapper.m
+++ tbsc_model_pem_wrapper.m
@@ -1,6 +1,6 @@
-function [ xdot, y_pem ]= tbsc_model_pem_wrapper(t,x,u, Jx_fac, Jy_fac, Jz_fac, elev_lift_fac, elev_drag_fac, varargin)
+function [ xdot, y_pem ] = tbsc_model_pem_wrapper(t,x,u, Jx_fac, Jy_fac, Jz_fac, elev_lift_fac, elev_drag_fac, body_x_drag_fac, varargin)
 
-  [xdot, y] = tbsc_model(t, x, u, Jx_fac, Jy_fac, Jz_fac, elev_lift_fac, elev_drag_fac, varargin);
+  [xdot, y] = tbsc_model(t, x, u, Jx_fac, Jy_fac, Jz_fac, elev_lift_fac, elev_drag_fac, body_x_drag_fac, varargin);
   
   y_pem = [ y(4); y(5); y(6) ];
   
