3db660039685fcb046f809f7ba9cc54fc732b1bb
Andy Barry <abarry@gmail.com>
2015-05-22 23:08:50 -0400

    small change.

--- BuildTrajectoryLibrary.m
+++ BuildTrajectoryLibrary.m
@@ -114,7 +114,8 @@ xtraj_draw = [x0 xf_turn];
 
 lib = AddLqrControllersToLib('right-turn', lib, xtraj_turn2, utraj_turn2, gains);
 
-lib = FindClimbTrimDrake(p, lib);
+max_climb = 1.0; % m/s
+lib = FindClimbTrimDrake(p, max_climb, lib);
 
 return;
 
